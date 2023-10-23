223379ae65cacf60a9bf48d1d1e1c73f37eebc7c
ByTheEfsane <59279876+oomerty@users.noreply.github.com>
2023-10-24 01:53:30 +0300

    01:53
    all questions are answered, comments are provided

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class Main {
     public static void main(String[] args) {
@@ -8,12 +9,33 @@ class Main {
             Yazılımda oluşabilecek kaç türlü hata vardır? Bunları açıklayınız. En az hata ile kod yazmak için ne yapılmalıdır?
         */
         /* 「AÇIKLAMA」
+            Yazılımda sayısız hata türü vardır ve en yaygın 7 tanesini şöyle sıralayabiliriz:
+            1- Syntax Hataları: İnsan dilleri gibi programlama dillerinin de grammarları vardır ve insanların aksine programlar bu hataları
+            kabul edememektedir. Yazılan dilin kurallarına uyulmalıdır.
+            2- Runtime Hataları: Bu tür hatalar programın farklı sistemlerde veya beklenmeyen bir şekilde kullanılmasından dolayı ortaya çıkabilir.
+            3- Logic/Mantık Hataları: Bu tür hatalar kodlama sırasında geliştiricinin beklediği sonucu alamamasına yol açacak mantık
+            hataları yapması sonucu ortaya çıkmaktadır.
+            4- Compilation/Derleme Hataları: Bu tür hatalar derleyicinin yazılan kodu düşük seviye koda çevirme sırasında sorun yaşaması
+            sonucu ortaya çıkarlar.
+            5- Arithmetic/Aritmerik Hatalar: Matematik içeren işlemlerde yaşanan sorunlardan dolayı çıkan hata türüdür.
+            6- Resource/Kaynak Hataları: Eğer program, bilgisayardan sahip olduğu bellekten ve kaynaklardan daha fazlasını zorla almak isterse
+            bu hatalarla karşılışılabilir.
+            7- Interface/Arayüz Hataları: Programın nasıl kullanılması gerektiği ve nasıl kullanıldığı arasında bir çelişki yaşanırsa bu hata
+            türü ile karşılışılabilir.
 
+            En az hata ile kod yazmak için:
+            * Planlama yapılmalıdır. Bir proje başında hedefsiz yola çıkılmaması için her adım planlanmalıdır.
+            * Test aşamasına önem gösterilmelidir. Kod yazıldıkça test edilmelidir ve ilerlendikçe sorun çıkarmaması için düzeltilmelidir.
+            Hata ayıklama araçları verimli şekilde kullanılmalıdır.
+            * Temiz kod prensiplerine önem gösterilmelidir.
+            * Belgelendirme işlemi yapılmalıdır.
+            * Geliştirici olarak olundupu gibi kalınmamalı, kendini geliştirmek için çaba verilmelidir ve güncel gelişmelerden haberdar olunmalıdır.
         */
-
-        //conditionedNum.conditionedMain();
-        //diceApp.dice();
-        //nubmerGuesser.randomGuesser();
+        diceApp.dice(); // 2. Soru
+        nubmerGuesser.randomGuesser(); // 3. Soru
+        conditionedNum.conditionedMain(); // 4. Soru
+        System.out.println(primeXMethod.isPrimeX(7877)); // 5. Soru
+        //primeXMethod.isPrimeX(7877);
     }
 }
 
@@ -24,7 +46,10 @@ class diceApp {
         programını  yazınız. Ama gelen  sayıları zar resmini gösterecek şekil deekranda gösteriniz. (Zar resimleri internetten alabilirsiniz.)
     */
     /* 「AÇIKLAMA」
-
+        * Çift zar simülasyonu,üç pencereden oluşmaktardır. İlk pencere işlevsizdir ama "OK" butonuna basıldıktan sonra
+        "randomNumGenerator" fonksiyonu ile iki zar için de ayrı rastgele sayı çekilmektedir.
+        * İkinci pencerede ilk zarın sonucu ve gelen sayıya göre zar resmi gösterilmektedir, resim "diceImg" fonksiyonundaki switch-case yapısıyla belirlenmektedir.
+        * Üçüncü pencerede ikinci zarın sonuçları önce pencerede olduğu gibi gösterilecektir.
     */
     public static void dice() {
         ImageIcon icon = new ImageIcon(diceApp.class.getResource("/img/dice.gif"));
@@ -64,7 +89,13 @@ class nubmerGuesser {
         Kullanıcı girişlerini Joptionpane.showmessagedialog,joptionpane.showinputdialogyapılarını kullanınız. (try-catch yapısını kullanınız)
     */
     /* 「AÇIKLAMA」
-
+        * Sayı bilmece oyunundan, bilgisayar tarafından 1 ile 100 arasında rastgele üretilen sayıyı ("randomNumGenerator" fonk ile üretilecektir.)
+        bilmeleri tanınan tahmin hakkı bitmeden istenecektir.
+        * İlk başta kullanıcı yarışmayla tanıştırılacaktır, ardından zorluk seviyesi seçmeleri istenecektir. Zorluk seviyesine göre tahmin hakkı verilecektir.
+        * İlk tahmin alınacaktır, eğer girilen değer sayı değilse terminalden uyarı verilecektir. Değer istenilen gibiyse değerlendirme başlayacaktır.
+        * Eğer tahminler istenilenden küçük ya da büyük ise bir sonraki tahmin penceresinde kullanıcıya yazı ile belirtilecektir.
+        * Doğru sayının tahmin edilmesi veya tahmin hakkının tüketilmesi sonucunda bitiş ekranı gelecektir ve ilgili bilgi gösterilecektir. Eğer kullanıcı
+        tekrar oynamak isterse işlem baştan başlayacaktır.
     */
     public static int randomNumGenerator(int min, int max) {
         int randomNum = (int)Math.floor(Math.random() * (max - min + 1) + min);
@@ -142,7 +173,12 @@ class conditionedNum {
         Yukarıdaki koşulların hepsini sağlayan tüm abc sayılarını bulan programı yazınız.
     */
     /* 「AÇIKLAMA」
-
+        * Bu çözümde üstte verilen koşulları sağlayan üç basamaklı sayılar bulunacaktır. Başlangıç değeri olarak en küçük üç basamaklı sayı 100 yazılmıştır.
+        while döngüsü ile 100'den başlayarak tüm üç basamaklı sayılar değerlendirilecektir.
+        * Döngü başında şuan bulunan sayı yüzlük, onluk ve birlik değerlerine ayrılmıştır. Ayrıca ters hali hesaplanmıştır.
+        * İç while döngüsünde eğer koşulları sağlayan bir sayı varsa konsola yazdırılacaktır. Sağlayıp sağlamaması bağımsız olarak döngü kırılacaktır
+        ve ardından sayı 1 arttırılacaktır.
+        * Asal olma şartları "isPrime" fonksiyonu ile kontrol edilecektir.
     */
     public static void conditionedMain() {
         int num = 100;
@@ -175,9 +211,27 @@ class primeXMethod {
         Eğer herhangi bir kademede asal olmayan bir sayı elde edilirse metot false değerine geri dönecektir.
     */
     /* 「AÇIKLAMA」
-
+        * "isPrimeX" fonksiyonu verilen argümanı değerlendirip şartlar sağlanması halinde true, sağlanmazsa false gönderecektir.
+        * Sayının tek basamaklı olana kadar işleme gireceği döngüye girilince en başta sayı geçici int'e atanmaktadır.
+        Sonrasında işlemlerin sonrası için boş bir ArrayList oluşturulmaktadır. Bu işlemler döngü kırılmadıkça her seferinde yapılacaktır.
+        * Ardından girilen sayının asal olup olmadığı kontrol edilmektedir ve asal ise basamaklarına ayrılmaktadır. Basamaklar toplanılıp, num
+        değişkeninin yerine geçkmektedir.
+        * Eğer sayı herhangi bir adımda asal olmazsa false dönecektir.
+        * Gİirlen sayıların asal olup olmadığı "conditionedNum" sınıfı altındaki "isPrime" fonksiyonu tarafından kontrol edilmektedir.
     */
-    public static void deneme() {
-        System.out.println("This is an example");
+    public static boolean isPrimeX(int num) {
+        while (num > 10) {
+            int tempNum = num;
+            ArrayList<Integer> arr = new ArrayList<Integer>();
+            if (conditionedNum.isPrime(num)) {
+                while (tempNum > 0) {
+                    arr.add(tempNum % 10);
+                    tempNum /= 10;
+                }
+                for (int nums: arr) tempNum += nums;
+            } else return false;
+            num = tempNum;
+        }
+        return true;
     }
 }
